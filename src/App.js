1c6bf62a582445c19e21684dd5ae42b5221a0ea7
Bradley Blue <b_blue@mail.fhsu.edu>
2024-03-21 00:01:01 -0500

    working except for cart page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './pages/Home';
+import ShopPage from './pages/ShopPage';
+import About from './pages/About';
+import CartPage from './pages/CartPage';
+import Nav from './Nav';
+/* import ProductDetailPage from './ProductDetailPage';
+import SearchPage from './SearchPage';
+import Thanks from './Thanks';
+import Missing from './Missing'; */
+import { DataProvider } from './context/DataContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataProvider>
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path='/' Component={Home} />
+          <Route path='/ShopPage' Component={ShopPage}/>
+          <Route path='/About' Component={About}/>
+          <Route path='/CartPage' Component={CartPage}/>
+        </Routes>
+      </Router>
+    </DataProvider>
   );
 }
 
