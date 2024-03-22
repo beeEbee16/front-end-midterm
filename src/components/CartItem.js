081368e60ff70f56d2402df148943793f06fcaa7
Bradley Blue <b_blue@mail.fhsu.edu>
2024-03-22 00:54:40 -0500

    Correct button placement for cart items. Still need product descriptions and working cart item buttons

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -3,6 +3,7 @@ import DataContext from '../context/DataContext';
 import { StoreProducts } from "../components/StoreProducts";
 import { RiSubtractFill } from "react-icons/ri";
 import { IoMdAdd } from "react-icons/io";
+import { MdDelete } from "react-icons/md";
 
 const CartItem = ({ id, quantity }) => {
     const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useContext(DataContext);
@@ -13,19 +14,20 @@ const CartItem = ({ id, quantity }) => {
 
   return (
     <div className='cartItem'>
-    <img src={item.productImage} />
-    <div className='cartDescription'>
-        <div id='cartItemName'><b>{item.productName}</b></div>
-        <div className='cartItemTotal'>
-            <p>${item.price.toFixed(2)} ea</p>
-            <p id='itemTotal'><b>${totalItemPrice.toFixed(2)}</b></p>
+        <img src={item.productImage} />
+        <div className='cartDescription'>
+            <div id='cartItemName'><b>{item.productName}</b></div>
+            <div className='cartItemTotal'>
+                <p>${item.price.toFixed(2)} ea</p>
+                <p id='itemTotal'><b>${totalItemPrice.toFixed(2)}</b></p>
+            </div>
+        <div className='cartItemCounter'>
+            <RiSubtractFill role='button' className='btnCartSub btnCartItem'/>
+            <p style={{fontSize: '20px'}}><b>{quantity}</b></p>
+            <IoMdAdd role='button' className='btnCartAdd btnCartItem'/>
+            <MdDelete role='button' className='btnCartDelete'/>
         </div>
-    </div>
-    {/* <div className='cartItemCounter'>
-        <RiSubtractFill role='button' className='btnCartSub btnCartItem'/>
-        <p style={{fontSize: '20px'}}><b>{quantity}</b></p>
-        <IoMdAdd role='button' className='btnCartAdd btnCartItem'/>
-    </div> */}
+        </div>
     </div>
 )
 }
